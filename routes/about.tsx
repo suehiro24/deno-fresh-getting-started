79c21016c85d73ccc1c17e819167cfd33d9f3616
HayataSato <hayatasato.0@gmail.com>
2022-07-03 18:30:07 +0900

    2.5. Custom handlers

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,5 +1,32 @@
 /** @jsx h */
 import { h } from "preact";
+// import { Handlers, Handler } from "$fresh/server.ts";
+
+// export const handler: Handlers = {
+//   // For Page component
+//   async GET(req, ctx) {
+//     const resp = await ctx.render();
+//     resp.headers.set("X-Custom-Header", "Hello");
+//     return resp;
+//   },
+//
+//   // For API route
+//   // GET(req, ctx) {
+//   //   const uuid = crypto.randomUUID();
+//   //   return new Response(JSON.stringify(uuid), {
+//   //     headers: { "Content-Type": "application/json" },
+//   //   });
+//   // },
+//
+// };
+
+// For API route (catch all methods)
+// export const handler: Handler = (req, ctx) => {
+//   const uuid = crypto.randomUUID();
+//   return new Response(JSON.stringify(uuid), {
+//     headers: { "Content-Type": "application/json" },
+//   });
+// };
 
 export default function AboutPage() {
   return (
